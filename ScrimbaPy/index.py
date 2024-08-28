e422b726cc40c9e8396dbedc37e0ae3826e183f1
Prajwal Gopal Poojary <prajwalgpy@gmail.com>
2024-08-28 18:47:53 +0530

    added

--- ScrimbaPy/index.py
+++ ScrimbaPy/index.py
@@ -1,27 +1,9 @@
-print('Guessing game') 
-# Guess the correct number in 3 guesses. If you don’t get it right after 3 guesses you lose the game. 
-# Give user input box: 1. To capture guesses, 
-# print(and input boxes) 1. If user wins 2. If user loses
-# Tip:( remember you won’t see  print statements durng execution, so If you want to see prints during whle loop, then print to the input box
+names = ['john ClEEse','Eric IDLE','michael']
+names1 = ['graHam chapman', 'TERRY', 'terry jones']
 
-#Modification 1: number 1-100, tell user if guess is too high/low ,and let them have 5-10 guesses.
-# Tip:( remember you won’t see  print statements during execution, so If you want to see prints during whle loop, print to the input box (This is specific to this platform)
-# Three Loop Questions:
-#1. What do I want to repeat?
-#  -> 
-#2. What do I want to change each time?
-#  -> 
-#3. How long should we repeat?
-#  -> 
+names.extend(names1)
+print(names)
+for frends in names:
+        print(f"{frends.capitalize()} ! you are invited to the party on saturdy.")
 
-number = 30
-gessCount =  3
-while gessCount > 0:
-    inputval = int(input("enter an Numeber"))
-    if number == inputval :
-        print("you win ")
-
-    else:
-        print("you lose")  
-
-    gessCount = gessCount -1
+ 
