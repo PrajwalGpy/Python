514e553c6a4fb0b547b3fc91e48f12c8d5370c7a
Prajwal Gopal Poojary <prajwalgpy@gmail.com>
2024-08-29 20:15:10 +0530

    added

--- ScrimbaPy/index.py
+++ ScrimbaPy/index.py
@@ -2,6 +2,10 @@ names = ['john ClEEse','Eric IDLE','michael']
 names1 = ['graHam chapman', 'TERRY', 'terry jones']
 
 names.extend(names1)
+
+
+for names1 in range(2):
+       names.append(input("Enter the names")) 
 print(names)
 for frends in names:
         print(f"{frends.capitalize()} ! you are invited to the party on saturdy.")
